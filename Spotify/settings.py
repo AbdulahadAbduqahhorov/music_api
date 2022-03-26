d2c13903cb66a851a021f4b1bc146e1be1820a3c
Abdulahad <abdulahadabduqahhorov2001@gmail.com>
2022-03-26 15:35:31 +0500

    Add Allowed Host to settings

--- Spotify/settings.py
+++ Spotify/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-567q2z*b+vhmrga8vbi!ev=jq#&k$ycoqx^bu#bcu=m-izjng#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
