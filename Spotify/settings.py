173444222ec8e93c5d1267011917c76f530337c1
Abdulahad <abdulahadabduqahhorov2001@gmail.com>
2022-03-30 14:24:54 +0500

    allowed hosts updated

--- Spotify/settings.py
+++ Spotify/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-567q2z*b+vhmrga8vbi!ev=jq#&k$ycoqx^bu#bcu=m-izjng#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
